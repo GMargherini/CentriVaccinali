b54f5281c5ad272c1ff3ba77f2eebcd698713d77
GMargherini <84579545+GMargherini@users.noreply.github.com>
2022-07-06 16:20:48 +0200

    Update Creazione db.sql

--- Database/Creazione db.sql
+++ Database/Creazione db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE centri_vaccinali(
-    nome VARCHAR(30) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     comune VARCHAR(35) NOT NULL,
     indirizzo VARCHAR(50) NOT NULL,
     tipo VARCHAR(20) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE vaccinati(
     ID_vaccinazione INTEGER PRIMARY KEY CHECK (ID_vaccinazione>=0 AND ID_vaccinazione<56535),
     codice_fiscale CHAR(16) UNIQUE NOT NULL CHECK(codice_fiscale ~ '[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]'),
     nome_cognome VARCHAR(40),
-    nome VARCHAR(30) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     comune VARCHAR(35) NOT NULL,
     data_vaccinazione DATE DEFAULT CURRENT_DATE,
     tipo_vaccino VARCHAR(20),
@@ -31,7 +31,7 @@ CREATE TABLE eventi_avversi(
     ID_vaccinazione INTEGER NOT NULL REFERENCES vaccinati,
     severita INTEGER CHECK(severita>=1 AND severita <=5),
     note VARCHAR(256),
-    nome VARCHAR(30) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     comune VARCHAR(35) NOT NULL,
     PRIMARY KEY(sintomo,ID_vaccinazione),
     FOREIGN KEY(nome,comune) REFERENCES centri_vaccinali
@@ -39,7 +39,7 @@ CREATE TABLE eventi_avversi(
 
 CREATE TABLE aggregazioni_eventi(
     sintomo VARCHAR(30) NOT NULL REFERENCES eventi_avversi,
-    nome VARCHAR(30) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     comune VARCHAR(35) NOT NULL,
     numero_segnalazioni INTEGER,
     media_severita NUMERIC,
