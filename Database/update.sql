6ff748dc928e846f1ac9b0ddd40f6ee7b7821a7d
GMargherini <84579545+GMargherini@users.noreply.github.com>
2022-07-09 02:09:38 +0200

    Update update.sql

--- Database/update.sql
+++ Database/update.sql
@@ -9,3 +9,13 @@ numero_segnalazioni=(SELECT COUNT(*)
 media_severita=(SELECT AVG(severita)
 	FROM eventi_avversi ea, centri_vaccinali cv
 	WHERE ea.nome=cv.nome AND ea.comune=cv.comune AND ae.sintomo=ea.sintomo)
+
+--update dati aggregati centri
+update centri_vaccinali cv
+set totale_segnalazioni=(select sum(numero_segnalazioni)
+			from aggregazioni_eventi
+			where nome=cv.nome and comune=cv.comune),
+	media_generale=(select sum(media_severita * numero_segnalazioni)/sum(numero_segnalazioni)
+			from aggregazioni_eventi
+			where nome=cv.nome and comune=cv.comune
+			group by sintomo)
