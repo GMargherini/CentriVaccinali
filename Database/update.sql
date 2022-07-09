37967a4eb7dde1f657ab7b4c32c55b06e110dd51
GMargherini <84579545+GMargherini@users.noreply.github.com>
2022-07-09 18:26:22 +0200

    Update update.sql

--- Database/update.sql
+++ Database/update.sql
@@ -1,14 +1,11 @@
 --update dati aggregati
 UPDATE aggregazioni_eventi ae
-SET sintomo=(SELECT sintomo
-  FROM eventi_avversi
-  GROUP BY sintomo),
-numero_segnalazioni=(SELECT COUNT(*)
-	FROM centri_vaccinali cv, eventi_avversi ea
-	WHERE ea.nome=cv.nome AND ea.comune=cv.comune),
-media_severita=(SELECT AVG(severita)
-	FROM eventi_avversi ea, centri_vaccinali cv
-	WHERE ea.nome=cv.nome AND ea.comune=cv.comune AND ae.sintomo=ea.sintomo)
+SET numero_segnalazioni=(SELECT COUNT(*)
+		FROM centri_vaccinali cv, eventi_avversi ea
+		WHERE ea.nome=cv.nome AND ea.comune=cv.comune),
+	media_severita=(SELECT AVG(severita)
+		FROM eventi_avversi ea, centri_vaccinali cv
+		WHERE ea.nome=cv.nome AND ea.comune=cv.comune AND ae.sintomo=ea.sintomo)
 
 --update dati aggregati centri
 update centri_vaccinali cv
