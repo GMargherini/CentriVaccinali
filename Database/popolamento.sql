fc9b097b7768075ece7964a532a1c6d3e20644e9
GMargherini <84579545+GMargherini@users.noreply.github.com>
2022-08-13 16:18:37 +0200

    Create popolamento.sql

---
+++ Database/popolamento.sql
@@ -0,0 +1,8 @@
+insert into centri_vaccinali values('Sede vaccinale di Varese','Varese','Viale O. Rossi 9 21100','ospedaliero',null,null);
+insert into centri_vaccinali values('Sede vaccinale di Arcisate','Arcisate','Via Campi Maggiori 23 21051','ospedaliero',null,null);
+insert into centri_vaccinali values('Sede vaccinale di Azzate','Azzate','Via Acquadro 6 21022','ospedaliero',null,null);
+insert into centri_vaccinali values('Sede vaccinale di Laveno Mombello','Laveno Mombello','Via Ceretti 8 21014','ospedaliero',null,null);
+insert into centri_vaccinali values('Sede vaccinale di Luino','Luino','Via Verdi 6 21016','ospedaliero',null,null);
+insert into centri_vaccinali values('Sede vaccinale di Malnate','Malnate','Piazza Libertà 1 21046','ospedaliero',null,null);
+insert into centri_vaccinali values('Sede vaccinale di Sesto Calende','Sesto Calende','Largo C. Dell''Acqua 1 21018','ospedaliero',null,null);
+insert into centri_vaccinali values('Sede vaccinale di Tradate','Tradate','Via Gradisca 16 21049','ospedaliero',null,null);
