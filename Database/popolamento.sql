a2bc7cc4d94601a6126c2f21b94c6c5d7d3e55e6
GMargherini <84579545+GMargherini@users.noreply.github.com>
2022-08-24 16:17:24 +0200

    Update popolamento.sql

--- Database/popolamento.sql
+++ Database/popolamento.sql
@@ -1,3 +1,4 @@
+--centri_vaccinali
 insert into centri_vaccinali values('Sede vaccinale di Varese','Varese','Viale O. Rossi 9 21100','ospedaliero',null,null);
 insert into centri_vaccinali values('Sede vaccinale di Arcisate','Arcisate','Via Campi Maggiori 23 21051','ospedaliero',null,null);
 insert into centri_vaccinali values('Sede vaccinale di Azzate','Azzate','Via Acquadro 6 21022','ospedaliero',null,null);
@@ -6,3 +7,8 @@ insert into centri_vaccinali values('Sede vaccinale di Luino','Luino','Via Verdi
 insert into centri_vaccinali values('Sede vaccinale di Malnate','Malnate','Piazza Libertà 1 21046','ospedaliero',null,null);
 insert into centri_vaccinali values('Sede vaccinale di Sesto Calende','Sesto Calende','Largo C. Dell''Acqua 1 21018','ospedaliero',null,null);
 insert into centri_vaccinali values('Sede vaccinale di Tradate','Tradate','Via Gradisca 16 21049','ospedaliero',null,null);
+--vaccinati
+insert into vaccinati values (0,'AAAAAA00A00A000A','Mario Rossi','Sede vaccinale di Varese','Varese',CURRENT_DATE,'AstraZeneca');
+insert into vaccinati values (1,'AAAAAA00A00A000B','Luigi Ferrari','Sede vaccinale di Varese','Varese',CURRENT_DATE,'Pfizer');
+insert into vaccinati values (2,'AAAAAA00A00A000C','Anna Verdi','Sede vaccinale di Varese','Varese',CURRENT_DATE,'AstraZeneca');
+insert into vaccinati values (3,'AAAAAA00A00A000D','Maria Bianchi','Sede vaccinale di Arcisate','Arcisate',CURRENT_DATE,'AstraZeneca');
