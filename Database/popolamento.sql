582ba12b84992e563f159d722a1e83c9e491703d
GMargherini <84579545+GMargherini@users.noreply.github.com>
2022-08-25 14:48:28 +0200

    Update popolamento.sql

--- Database/popolamento.sql
+++ Database/popolamento.sql
@@ -12,3 +12,22 @@ insert into vaccinati values (0,'AAAAAA00A00A000A','Mario Rossi','Sede vaccinale
 insert into vaccinati values (1,'AAAAAA00A00A000B','Luigi Ferrari','Sede vaccinale di Varese','Varese',CURRENT_DATE,'Pfizer');
 insert into vaccinati values (2,'AAAAAA00A00A000C','Anna Verdi','Sede vaccinale di Varese','Varese',CURRENT_DATE,'AstraZeneca');
 insert into vaccinati values (3,'AAAAAA00A00A000D','Maria Bianchi','Sede vaccinale di Arcisate','Arcisate',CURRENT_DATE,'AstraZeneca');
+insert into vaccinati values(4,	'AAAAAA00A00A000E',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'AstraZeneca');
+insert into vaccinati values(5,	'AAAAAA00A00A000F',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'Pfizer');
+insert into vaccinati values(6,	'AAAAAA00A00A000G',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'Moderna');
+insert into vaccinati values(7,	'AAAAAA00A00A000H',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'AstraZeneca');
+insert into vaccinati values(8,	'AAAAAA00A00A000I',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'Pfizer');
+insert into vaccinati values(9,	'AAAAAA00A00A000J',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'Moderna');
+insert into vaccinati values(10,	'AAAAAA00A00A000K',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'AstraZeneca');
+insert into vaccinati values(11,	'AAAAAA00A00A000L',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'Pfizer');
+insert into vaccinati values(12,	'AAAAAA00A00A000M',	'Nome Cognome',	'Sede vaccinale di Varese',	'Varese',	current_date,	'Moderna');
+
+insert into vaccinati values(13,	'AAAAAA00A00A000N',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'AstraZeneca');
+insert into vaccinati values(14,	'AAAAAA00A00A000O',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'Pfizer');
+insert into vaccinati values(15,	'AAAAAA00A00A000P',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'Moderna');
+insert into vaccinati values(16,	'AAAAAA00A00A000Q',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'AstraZeneca');
+insert into vaccinati values(17,	'AAAAAA00A00A000R',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'Pfizer');
+insert into vaccinati values(18,	'AAAAAA00A00A000S',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'Moderna');
+insert into vaccinati values(19,	'AAAAAA00A00A000T',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'AstraZeneca');
+insert into vaccinati values(20,	'AAAAAA00A00A000U',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'Pfizer');
+insert into vaccinati values(21,	'AAAAAA00A00A000V',	'Nome Cognome',	'Sede vaccinale di Arcisate',	'Arcisate',	current_date,	'Moderna');
